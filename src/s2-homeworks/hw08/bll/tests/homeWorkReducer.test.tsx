f1d58825d8d701cccb2ea6c03d5fe7f3e7e3cf93
Ivan Shirykhalov <ivanshirykhalov@gmail.com>
2023-01-20 22:51:11 +0300

    hw8

--- src/s2-homeworks/hw08/bll/tests/homeWorkReducer.test.tsx
+++ src/s2-homeworks/hw08/bll/tests/homeWorkReducer.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { homeWorkReducer } from '../homeWorkReducer'
-import { UserType } from '../../HW8'
+import {homeWorkReducer} from '../homeWorkReducer'
+import {UserType} from '../../HW8'
 
 let initialState: UserType[]
 
